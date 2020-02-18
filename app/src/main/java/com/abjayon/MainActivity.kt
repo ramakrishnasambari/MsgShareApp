70d3006733af9880c022a1e61f882679a5e6a5d4
ramakrishnasambari <ramakrishna469@gmail.com>
2020-02-18 12:52:31 +0530

    Button activity

---
+++ app/src/main/java/com/abjayon/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.abjayon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("Main Activity", "This is test log.")
+
+            Toast.makeText(this, "Button was clicked.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
