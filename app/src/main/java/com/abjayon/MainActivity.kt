00d4d0922a1691eaff363e4ee83219ec130cc4a8
ramakrishnasambari <ramakrishna469@gmail.com>
2020-02-18 15:08:56 +0530

    Button activity

--- app/src/main/java/com/abjayon/MainActivity.kt
+++ app/src/main/java/com/abjayon/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.abjayon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -15,7 +16,16 @@ class MainActivity : AppCompatActivity() {
         btnShowToast.setOnClickListener {
             Log.i("Main Activity", "This is test log.")
 
-            Toast.makeText(this, "Button was clicked.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Button1 was clicked.", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener{
+            val message = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
